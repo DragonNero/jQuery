72f50dcfe5c65a93585135ac12542d73b4947f1e
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-31 22:39:48 +0200

    started jQuery

---
+++ index.js
@@ -0,0 +1,11 @@
+$("h1").click(function() {
+  $("h1").css("color", "purple");
+});
+$("button").click(function(){
+  $("h1").hide();
+});
+
+// $(document).keydown(function(event) {
+//   console.log(event.key);
+//   $("h1").text(event.key);
+// });
